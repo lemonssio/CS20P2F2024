6ab346c88773e7b7fd9d45563e0104e4fdd84061
Denise <Denise@10.0.0.165>
2024-09-13 22:39:06 -0600

    10:38PM

--- Chapter3/src/Skillbuilders/gradeaveragePT2.java
+++ Chapter3/src/Skillbuilders/gradeaveragePT2.java
@@ -1,31 +1,22 @@
 package Skillbuilders;
-
 import java.util.Scanner;
 
-public class gradeaverage1n2 {
-
-	public static void main(String[] args) {
+public class gradeaveragePT2 {
 	Scanner userinput = new Scanner(System.in);
-		
-	System.out.println("hello please enter your five grades and i'll give you the average!!");
+{
+	System.out.println("hello please enter your five grades and i'll give you the average!");
 	
 	System.out.print("please enter your first grade: ");
 	int grade1 = userinput.nextInt();
 	System.out.print("please enter your second grade: ");
-	int grade2 = userinput.nextInt();
+	int grade2 = userinput.nextInt(); 
+	int grade1n2 = (grade1 += grade2);
 	System.out.print("please enter your third grade: ");
 	int grade3 = userinput.nextInt();
 	System.out.print("please enter your fourth grade: ");
 	int grade4 = userinput.nextInt();
+	int grade3n4 = (grade3 += grade4);
 	System.out.print("please enter your fifth grade: ");
 	int grade5 = userinput.nextInt();
-	
-	int wholegrade = ((grade1 + grade2 + grade3 + grade4 + grade5) / 5 );
-
-	System.out.print("your average grade is %" + wholegrade);
-	
-	
-	
-	}
-
-}
+	System.out.println((grade5 + grade3n4 + grade1n2) / 5);
+}}
---
+++ Chapter3/src/mastery/masterydigits.java
@@ -0,0 +1,5 @@
+package mastery;
+
+public class masterydigits {
+
+}
