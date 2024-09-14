4f1c1826a656ff5f23fa3649013413d4de1de75c
Denise <Denise@DESKTOP-A5P01B2>
2024-09-13 23:46:51 -0600

    11:46PM

--- Chapter3/src/Skillbuilders/gradeavePT2.java
+++ Chapter3/src/Skillbuilders/gradeavePT2.java
@@ -1,11 +1,13 @@
 package Skillbuilders;
+
 import java.util.Scanner;
 
-public class gradeaveragePT2 {
+public class gradeavePT2 {
+
+	public static void main(String[] args) {
 	Scanner userinput = new Scanner(System.in);
-{
-	System.out.println("hello please enter your five grades and i'll give you the average!");
-	//
+		//
+	System.out.println("hello please enter your five grades and i'll give you the average!!");
 	System.out.print("please enter your first grade: ");
 	int grade1 = userinput.nextInt();
 	System.out.print("please enter your second grade: ");
@@ -18,5 +20,5 @@ public class gradeaveragePT2 {
 	int grade3n4 = (grade3 += grade4);
 	System.out.print("please enter your fifth grade: ");
 	int grade5 = userinput.nextInt();
-	System.out.println((grade5 + grade3n4 + grade1n2) / 5);
+	System.out.println("Your average is: %" + (grade5 + grade3n4 + grade1n2) / 5 );
 }}
